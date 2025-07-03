6cc85135a437e123d0e95e6bd82461a68864a69d
joey102522 <joeykm91@gmail.com>
2025-07-03 14:43:14 -0400

    fix: centered homepage h1 text for responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="flex items-center justify-center h-dvh bg-primary">
         <div className="flex items-center justify-center flex-col gap-8">
             <div className="flex justify-center gap-6 flex-col items-center text-primary-foreground">
-                <h1 className="text-6xl font-bold">Welcome to DevBoard</h1>
+                <h1 className="text-6xl font-bold text-center">Welcome to DevBoard</h1>
                 <p>Start tracking your jobs now!</p>
             </div>
             <GetStartedButton />
