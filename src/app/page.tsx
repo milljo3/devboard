e54fe59e081f9c70e90d1c255de9b7389b48d08e
joey102522 <joeykm91@gmail.com>
2025-07-03 15:01:39 -0400

    fix: moved homepage background coloring to the entire site layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import GetStartedButton from "@/components/GetStartedButton";
 
 export default function Home() {
   return (
-      <div className="flex items-center justify-center h-dvh bg-primary">
+      <div className="flex items-center justify-center h-dvh">
         <div className="flex items-center justify-center flex-col gap-8">
             <div className="flex justify-center gap-6 flex-col items-center text-primary-foreground">
                 <h1 className="text-6xl font-bold text-center">Welcome to DevBoard</h1>
