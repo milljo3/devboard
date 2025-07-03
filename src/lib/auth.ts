b02899e005337dc7e7fd7f40c4af8203c0035d22
joey102522 <joeykm91@gmail.com>
2025-07-03 00:47:46 -0400

    fix: fixed auth.ts prisma client route

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "../../node_modules/@prisma/client";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
