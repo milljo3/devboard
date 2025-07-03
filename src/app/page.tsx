05d709c645852f445d5d6ec8300e9ca48d152ce4
joey102522 <joeykm91@gmail.com>
2025-07-03 14:10:05 -0400

    feat: Welcome page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import GetStartedButton from "@/components/GetStartedButton";
+
 export default function Home() {
   return (
-      <div>
-        Home
+      <div className="flex items-center justify-center h-dvh bg-primary">
+        <div className="flex items-center justify-center">
+            <div className="flex justify-center gap-6 flex-col items-center text-primary-foreground">
+                <h1 className="text-6xl font-bold">Welcome to DevBoard</h1>
+                <p>Start tracking your jobs now!</p>
+            </div>
+            <GetStartedButton />
+        </div>
       </div>
   )
 }
---
+++ src/components/GetStartedButton.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const GetStartedButton = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default GetStartedButton;
