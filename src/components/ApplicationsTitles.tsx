eee0b14d9379f508b702307df080fbd92b7ad4b0
joey102522 <joeykm91@gmail.com>
2025-07-05 12:51:56 -0400

    feat: added application column headers component

---
+++ src/components/ApplicationsTitles.tsx
@@ -0,0 +1,26 @@
+const ApplicationsTitles = () => {
+    return (
+        <div className="
+          grid
+          w-full
+          px-4
+          py-2
+          text-sm
+          font-medium
+          border-b
+          text-muted-foreground
+          grid-cols-[3fr_3fr_2fr]
+          md:grid-cols-[2fr_2fr_1fr_1fr_1fr]
+          justify-center
+        ">
+            <span>Company</span>
+            <span>Position</span>
+            <span className="md:hidden">Status</span>
+            <span className="hidden md:block">Applied</span>
+            <span className="hidden md:block">Updated</span>
+            <span className="hidden md:block">Status</span>
+        </div>
+    );
+};
+
+export default ApplicationsTitles;
