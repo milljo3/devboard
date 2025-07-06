5f268483440aaf2e72ddacc47a938479123f5997
joey102522 <joeykm91@gmail.com>
2025-07-05 21:45:57 -0400

    fix: added type for PATCH and DELETE applications params

--- src/app/api/applications/[id]/route.ts
+++ src/app/api/applications/[id]/route.ts
@@ -2,14 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { auth } from "@/lib/auth";
 
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+interface Params {
+    params: {
+        id: string;
+    };
+}
+
+export async function PATCH(req: NextRequest, context: Params) {
     const session = await auth.api.getSession({ headers: req.headers });
 
     if (!session?.user) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const id = params.id;
+    const id = context.params.id;
     const body = await req.json();
 
     const { company, companyUrl, position, applicationUrl, status } = body;
@@ -38,14 +44,15 @@ export async function PATCH(req: NextRequest, { params }: { params: { id: string
     }
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+
+export async function DELETE(req: NextRequest, context: Params) {
     const session = await auth.api.getSession({ headers: req.headers });
 
     if (!session?.user) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const id = params.id;
+    const id = context.params.id;
 
     try {
         const existingApp = await prisma.application.findUnique({ where: { id } });
