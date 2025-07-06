29b158e10d2181f3dd7fefeecfe18dd79cecf365
joey102522 <joeykm91@gmail.com>
2025-07-06 15:56:24 -0400

    feat: added react query for handling page changes and sorting

--- src/components/Applications.tsx
+++ src/components/Applications.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import {useEffect, useState} from "react";
 import {Application} from "@/types/applications";
 import {ApplicationCard} from "@/components/ApplicationCard";
 import SearchApplicationForm from "@/components/SearchApplicationForm";
@@ -9,111 +8,82 @@ import {ApplicationDialog} from "@/components/ApplicationDialog";
 import {useRouter, useSearchParams} from "next/navigation";
 import {toast} from "sonner";
 import ApplicationsTitles from "@/components/ApplicationsTitles";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 
 const PAGE_SIZE = 8;
 
 const Applications = () => {
-    const [applications, setApplications] = useState<Application[]>([]);
-    const [totalCount, setTotalCount] = useState(0);
-    const [isPending, setIsPending] = useState(false);
-
     const searchParams = useSearchParams();
     const page = parseInt(searchParams.get("page") || "1");
+    const sortField = searchParams.get("sortField") || "updatedAt";
+    const sortOrder = searchParams.get("sortOrder") || "desc";
 
     const router = useRouter();
+    const queryClient = useQueryClient();
 
-    const fetchApplications = async () => {
-        setIsPending(true);
-        try {
-            const response = await fetch(`/api/applications?page=${page}`);
-            const data = await response.json();
-            setApplications(data.applications);
-            setTotalCount(data.totalCount);
-            return data;
-        }
-        catch (error) {
-            console.error(error);
-            return null;
-        }
-        finally {
-            setIsPending(false);
-        }
+    const queryKey = ["applications", {page, sortField, sortOrder}];
+
+    const {data, isLoading,} = useQuery({
+        queryKey,
+        queryFn: async () => {
+            const res = await fetch(`/api/applications?page=${page}&sortField=${sortField}&sortOrder=${sortOrder}`);
+            if (!res.ok) throw new Error("Failed to fetch applications.");
+            return res.json();
+        },
+    });
+
+    const totalPages = Math.ceil((data?.totalCount || 0) / PAGE_SIZE);
+
+    if (data?.applications.length === 0 && page > 1 && totalPages > 0) {
+        router.push(`?page=${totalPages}`);
     }
 
-    useEffect(() => {
-        const load = async () => {
-            const data = await fetchApplications();
-
-            const totalPages = Math.ceil((data?.totalCount || 0) / PAGE_SIZE);
-
-            if (data && page > totalPages && totalPages > 0) {
-                router.push(`?page=${totalPages}`);
-            }
-        };
-        load();
-    }, [page])
-
-    const handleAddApplication = () => {
-        router.push("?page=1");
-        fetchApplications();
-    }
-
-    const handleEditApplication = async (application?: Application) => {
-        if (!application) return;
-
-        const previousApplications = [...applications];
-        const index = previousApplications.findIndex(app => app.id === application.id);
-
-        if (index === -1) return;
-
-        const newApplications = [...previousApplications];
-        newApplications[index] = application;
-        setApplications(newApplications);
-
-        try {
+    const editMutation = useMutation({
+        mutationFn: async (application: Partial<Application>) => {
             const res = await fetch(`/api/applications/${application.id}`, {
                 method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                },
+                headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(application),
             });
-
-            if (!res.ok) throw new Error();
-
+            if (!res.ok) throw new Error("Failed to update application.");
+            return res.json();
+        },
+        onSuccess: () => {
             toast.success("Application updated successfully.");
-
-            await fetchApplications();
+            queryClient.invalidateQueries({queryKey: ["applications"]});
+        },
+        onError: () => {
+            toast.error("Failed to update application.");
         }
-        catch (error) {
-            console.error(error);
-            toast.error("Error adding application");
-            setApplications(previousApplications);
-        }
-    }
+    });
 
-    const handleDeleteApplication = async (applicationId: string) => {
-        try {
+    const deleteMutation = useMutation({
+        mutationFn: async (applicationId: string) => {
             const res = await fetch(`/api/applications/${applicationId}`, {
                 method: "DELETE",
             });
-
-            if (!res.ok) throw new Error();
-
+            if (!res.ok) throw new Error("Failed to delete application.");
+        },
+        onSuccess: () => {
             toast.success("Application deleted successfully.");
-
-            const data = await fetchApplications();
-
-            const totalPages = Math.ceil((data?.totalCount || 0) / PAGE_SIZE);
-
-            if (data && data.applications.length === 0 && page > 1) {
-                router.push(`?page=${totalPages}`);
-            }
+            queryClient.invalidateQueries({queryKey: ["applications"]});
+        },
+        onError: () => {
+            toast.error("Failed to delete application.");
         }
-        catch (error) {
-            console.error(error);
-            toast.error("Error deleting application");
-        }
+    })
+
+    const handleAddApplication = () => {
+        router.push("?page=1");
+        queryClient.invalidateQueries({queryKey: ["applications"]});
+    }
+
+    const handleEditApplication = async (updatedFields: Partial<Application>) => {
+        editMutation.mutate(updatedFields);
+    }
+
+    const handleDeleteApplication = async (applicationId: string) => {
+        deleteMutation.mutate(applicationId)
     };
 
 
@@ -121,22 +91,20 @@ const Applications = () => {
 
     }
 
-    const totalPages = Math.ceil(totalCount / PAGE_SIZE);
-
     return (
         <div className="w-full flex flex-col items-center gap-4 mt-4">
             <div className="flex gap-4">
-                <ApplicationDialog onSave={handleAddApplication} mode="add"/>
-                <SearchApplicationForm onSearch={handleSearch} isPending={isPending} />
+                <ApplicationDialog onAdd={handleAddApplication} mode="add"/>
+                <SearchApplicationForm onSearch={handleSearch} isPending={isLoading} />
             </div>
-            {isPending ? (
+            {isLoading ? (
                 <div>Loading...</div>
             ) : (
                 <>
 
                     <div className="grid grid-cols-1 gap-2 place-items-center md:w-2/3 w-full">
                         <ApplicationsTitles />
-                        {applications.map((application: Application) => (
+                        {data?.applications.map((application: Application) => (
                             <ApplicationCard
                                 application={application}
                                 key={application.id}
