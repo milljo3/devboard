66bc9452372257a3d3aa714fda309772098c20ac
joey102522 <joeykm91@gmail.com>
2025-07-08 15:19:00 -0400

    feat: added github signature with link to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import GetStartedButton from "@/components/GetStartedButton";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +11,16 @@ export default function Home() {
                 <p>Start tracking your jobs now!</p>
             </div>
             <GetStartedButton />
+            <div className="flex items-center justify-center flex-col gap-1">
+                <small>Created by</small>
+                <Button variant="outline" className="flex items-center justify-center">
+                    <Link href="https://github.com/milljo3/devboard" className="flex items-center gap-1">
+                        <img src="https://avatars.githubusercontent.com/u/144623594?v=4" alt="GitHub Profile Picture" width="25" height="25" />
+                        <p>@milljo3</p>
+                        <img src="/github.svg" alt="GitHub Icon" width="25" height="25" />
+                    </Link>
+                </Button>
+            </div>
         </div>
       </div>
   )
